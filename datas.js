79eaaf9354f6103d223a10a5c5c9bfe15682052d
Yab1 <yabilisanu@gmail.com>
2023-01-31 21:30:45 +0300

    added a new form for step two

---
+++ datas.js
@@ -0,0 +1,23 @@
+const data = [
+  {
+    id: 1,
+    heading: 'Personal info',
+    description: 'Please provide your name,email address, and phone number',
+  },
+  {
+    id: 2,
+    heading: 'Select your plan',
+    description: 'You have the option of monthly or yearly billing',
+  },
+  {
+    id: 3,
+    heading: 'Pick add-ons',
+    description: 'Add-ons help enhance your gaming experience',
+  },
+  {
+    id: 4,
+    heading: 'Finishing up',
+    description: 'Double-check everything looks Ok before confirming',
+  },
+];
+export default data;
---
+++ script.js
@@ -0,0 +1,62 @@
+'useStrict';
+//Query Selector
+const qs = (sel) => document.querySelector(sel);
+const qsa = (sel) => document.querySelectorAll(sel);
+// Dom elements
+const step = qsa('.step');
+const header2 = qs('h2');
+const para = qs('.description');
+const nextBtn = qs('.btn--next');
+const prvBtn = qs('.btn--go-back');
+const labels = qsa('label');
+const plans = qsa('.plan');
+console.log(plans);
+//Heading 2 text content array
+const data = [
+  {
+    id: 1,
+    heading: 'Personal info',
+    description: 'Please provide your name, email address, and phone number',
+  },
+  {
+    id: 2,
+    heading: 'Select your plan',
+    description: 'You have the option of monthly or yearly billing',
+  },
+  {
+    id: 3,
+    heading: 'Pick add-ons',
+    description: 'Add-ons help enhance your gaming experience',
+  },
+  {
+    id: 4,
+    heading: 'Finishing up',
+    description: 'Double-check everything looks Ok before confirming',
+  },
+];
+let count = 0;
+//Functions
+const action = function (count) {
+  header2.textContent = data[count].heading;
+  para.textContent = data[count].description;
+  step.forEach((ele) => ele.classList.remove('step-active'));
+  step[count].classList.add('step-active');
+  if (!count == '0') {
+    labels.forEach((label) => label.classList.add('hidden'));
+  } else {
+    labels.forEach((label) => label.classList.remove('hidden'));
+  }
+};
+//EventListeners
+nextBtn.addEventListener('click', () => {
+  if (count < 3) {
+    count++;
+    action(count);
+  }
+});
+prvBtn.addEventListener('click', () => {
+  if (count >= 1) {
+    count--;
+    action(count);
+  }
+});
